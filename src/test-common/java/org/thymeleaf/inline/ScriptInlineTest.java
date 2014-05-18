99e909fb26d22cfb37a190f7f9d42e749ba38f12
Daniel Fernández <daniel.fernandez@11thlabs.org>
2014-05-18 15:42:48 +0200

    Added inlining tests.

---
+++ src/test-common/java/org/thymeleaf/inline/ScriptInlineTest.java
@@ -0,0 +1,92 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2012, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.inline;
+
+import java.io.StringWriter;
+import java.util.Collections;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.templateresolver.ITemplateResolver;
+import org.thymeleaf.util.DateUtils;
+
+public class ScriptInlineTest {
+
+
+
+    private static void testInlineResult(final String script, final String expectedResult) {
+        testInlineResult(script, expectedResult, Collections.<String, Object>emptyMap());
+    }
+
+    private static void testInlineResult(final String script, final String expectedResult, final String variableName, final Object variableValue) {
+        testInlineResult(script, expectedResult, Collections.singletonMap(variableName, variableValue));
+    }
+
+    private static void testInlineResult(final String script, final String expectedResult, final Map<String,Object> variables) {
+
+        final String completeScript =
+                "<script th:inline=\"javascript\">\n/*<![CDATA[ */\n" +
+                script +
+                "\n/* ]]> */\n</script>";
+
+        final ITemplateResolver templateResolver = new StringTemplateResolver(completeScript);
+        final TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+
+        final Context ctx = new Context();
+        ctx.setVariables(variables);
+
+        final StringWriter stringWriter = new StringWriter();
+        templateEngine.process("test-template", ctx, stringWriter);
+
+        final String result = stringWriter.toString();
+        final String extractedResult =
+                result.substring(0, result.indexOf("\n/* ]]> */\n</script>")).substring(24);
+
+
+        Assert.assertEquals(expectedResult, extractedResult);
+
+    }
+
+
+    @Test
+    public void testDateInline() throws Exception {
+
+        testInlineResult(
+                "[[${a}]]",
+                "'something'",
+                "a", "something");
+
+        testInlineResult(
+                "   /*[[${a}]]*/ 'prototype';",
+                "   'something';",
+                "a", "something");
+
+        testInlineResult(
+                "   /*[[${d}]]*/ 'prototype';",
+                "   '2013-01-01T14:30:00.000+01:00';",
+                "d", DateUtils.create(Integer.valueOf(2013),Integer.valueOf(01),Integer.valueOf(01),Integer.valueOf(14),Integer.valueOf(30)));
+
+
+    }
+}
