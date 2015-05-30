1eba0bcb9d480629c7f6821489bf896108bc7927
Daniel Fernández <daniel.fernandez@11thlabs.org>
2015-05-30 15:46:21 +0200

    Made room for the new org.thymeleaf.engine package in 3.0

---
+++ src/test/java/org/thymeleaf/templateengine/aggregation/dialect/Dialect02TextProcessor.java
@@ -0,0 +1,47 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2014, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.templateengine.aggregation.dialect;
+
+import org.thymeleaf.Arguments;
+import org.thymeleaf.dom.AbstractTextNode;
+import org.thymeleaf.processor.ProcessorResult;
+import org.thymeleaf.processor.TextNodeProcessorMatcher;
+import org.thymeleaf.processor.text.AbstractTextNodeProcessor;
+
+public class Dialect02TextProcessor extends AbstractTextNodeProcessor {
+
+
+    public Dialect02TextProcessor() {
+        super(new TextNodeProcessorMatcher());
+    }
+
+    @Override
+    public int getPrecedence() {
+        return 200;
+    }
+
+
+
+    @Override
+    protected ProcessorResult processTextNode(final Arguments arguments, final AbstractTextNode textNode) {
+        textNode.setContent(textNode.getContent() + "[02]");
+        return ProcessorResult.OK;
+    }
+}
