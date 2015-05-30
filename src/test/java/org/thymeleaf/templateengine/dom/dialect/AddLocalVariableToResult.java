c9ff89eec052fe858c7a0d36a17ec751b5452bce
Daniel Fernández <daniel.fernandez@11thlabs.org>
2015-05-30 13:38:17 +0200

    Merged some 3.0 tests, made room for the new org.thymeleaf.engine package in 3.0

---
+++ src/test/java/org/thymeleaf/templateengine/dom/dialect/AddLocalVariableToResult.java
@@ -0,0 +1,85 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2014, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.templateengine.dom.dialect;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.thymeleaf.Arguments;
+import org.thymeleaf.Configuration;
+import org.thymeleaf.dom.Element;
+import org.thymeleaf.processor.ProcessorResult;
+import org.thymeleaf.processor.attr.AbstractAttrProcessor;
+import org.thymeleaf.standard.expression.Assignation;
+import org.thymeleaf.standard.expression.AssignationSequence;
+import org.thymeleaf.standard.expression.AssignationUtils;
+import org.thymeleaf.standard.expression.IStandardExpression;
+
+public class AddLocalVariableToResult extends AbstractAttrProcessor {
+
+    
+    public AddLocalVariableToResult() {
+        super("add-local-variable-to-result");
+    }
+
+    
+    
+    
+    @Override
+    public int getPrecedence() {
+        return 1000;
+    }
+    
+
+    
+
+    @Override
+    protected ProcessorResult processAttribute(final Arguments arguments,
+            final Element element, final String attributeName) {
+
+        final String attributeValue = element.getAttributeValue(attributeName);
+
+        final Configuration configuration = arguments.getConfiguration();
+
+        final AssignationSequence assignationSequence =
+                AssignationUtils.parseAssignationSequence(configuration, arguments, attributeValue, false);
+
+        final Map<String,Object> localVariables = new HashMap<String,Object>();
+        for (final Assignation assignation : assignationSequence.getAssignations()) {
+
+            final IStandardExpression varNameExpr = assignation.getLeft();
+            final IStandardExpression varValueExpr = assignation.getRight();
+
+            final Object varName = varNameExpr.execute(configuration, arguments);
+            final Object varValue = varValueExpr.execute(configuration, arguments);
+
+            localVariables.put((varName == null? null : varName.toString()), varValue);
+
+        }
+        
+        element.removeAttribute(attributeName);
+        
+        return ProcessorResult.setLocalVariables(localVariables);
+        
+    }
+
+
+
+}
---
+++ src/test/java/org/thymeleaf/templateparser/HtmlBlockSelectorMarkupHandlerTest.java
@@ -0,0 +1,136 @@
+/*
+ * =============================================================================
+ *
+ *   Copyright (c) 2012-2014, The ATTOPARSER team (http://www.attoparser.org)
+ *
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ *
+ * =============================================================================
+ */
+package org.thymeleaf.templateparser;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStreamReader;
+import java.io.StringWriter;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Locale;
+
+import junit.framework.TestCase;
+import org.apache.commons.io.IOUtils;
+import org.apache.commons.lang3.StringUtils;
+import org.thymeleaf.IEngineConfiguration;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.TestTemplateEngineConfigurationBuilder;
+import org.thymeleaf.engine.ITemplateHandler;
+import org.thymeleaf.engine.OutputTemplateHandler;
+import org.thymeleaf.aurora.context.StandardProcessingContextFactory;
+import org.thymeleaf.resource.StringResource;
+import org.thymeleaf.templatemode.TemplateMode;
+
+/*
+ *
+ * @author Daniel Fernandez
+ * @since 2.0.0
+ */
+public class HtmlBlockSelectorMarkupHandlerTest extends TestCase {
+
+    private static final String RESOURCES_FOLDER = "templateparser/htmlblockselector/";
+    private static final IEngineConfiguration TEMPLATE_ENGINE_CONFIGURATION = TestTemplateEngineConfigurationBuilder.build();
+
+    public HtmlBlockSelectorMarkupHandlerTest() {
+        super();
+    }
+
+
+    public void test() throws Exception {
+
+        final HTMLTemplateParser parser = new HTMLTemplateParser(2, 4096);
+        final IEngineConfiguration templateEngineContext = TestTemplateEngineConfigurationBuilder.build();
+
+        final URL resourcesFolderURL = Thread.currentThread().getContextClassLoader().getResource(RESOURCES_FOLDER);
+        assertNotNull(resourcesFolderURL);
+
+        final File resourcesFolder = new File(resourcesFolderURL.toURI());
+        assertTrue(resourcesFolder.isDirectory());
+
+        final List<File> resourceFolderFiles = new ArrayList<File>(Arrays.asList(resourcesFolder.listFiles()));
+        Collections.sort(resourceFolderFiles, new Comparator<File>() {
+            public int compare(final File o1, final File o2) {
+                return o1.getName().compareTo(o2.getName());
+            }
+        });
+
+        final List<File> testFiles = new ArrayList<File>();
+        final List<File> resultFiles = new ArrayList<File>();
+
+        for (final File resourceFolderFile : resourceFolderFiles) {
+            if (resourceFolderFile.getName().startsWith("test") && resourceFolderFile.getName().endsWith(".html")) {
+                testFiles.add(resourceFolderFile);
+            } else if (resourceFolderFile.getName().startsWith("result") && resourceFolderFile.getName().endsWith(".html")) {
+                resultFiles.add(resourceFolderFile);
+            }
+        }
+
+        for (int i = 0; i < testFiles.size(); i++) {
+
+            final File testFile = testFiles.get(i);
+            final File resultFile = resultFiles.get(i);
+
+            final List<String> testFileLines =
+                    IOUtils.readLines(new InputStreamReader(new FileInputStream(testFile), "UTF-8"));
+            final String testFileContents = StringUtils.join(testFileLines,'\n');
+
+            final List<String> resultFileLines =
+                    IOUtils.readLines(new InputStreamReader(new FileInputStream(resultFile), "UTF-8"));
+            final String blockSelector = resultFileLines.get(0);
+            resultFileLines.remove(0);
+            final String resultFileContents = StringUtils.join(resultFileLines,'\n');
+
+            final String[] blockSelectors = StringUtils.split(blockSelector,",");
+
+            check(parser, templateEngineContext, testFile.getName(), testFileContents, resultFileContents, blockSelectors);
+
+        }
+
+
+    }
+
+
+
+
+
+    private static void check(
+            final HTMLTemplateParser parser, final IEngineConfiguration templateEngineContext,
+            final String templateName, final String input, final String output, final String[] blockSelectors)
+            throws Exception{
+
+        final StringWriter writer = new StringWriter();
+        final ITemplateHandler handler = new OutputTemplateHandler(writer);
+        handler.setProcessingContext(
+                StandardProcessingContextFactory.build(TEMPLATE_ENGINE_CONFIGURATION, templateName, TemplateMode.HTML, new Context(Locale.US)));
+
+        parser.parse(templateEngineContext, TemplateMode.HTML, new StringResource(templateName, input), blockSelectors, handler);
+
+        assertEquals("Test failed for file: " + templateName, output, writer.toString());
+
+    }
+
+
+
+}
