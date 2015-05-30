724b5fe30cea4aaa937d2851758a5bb1352d76b4
Daniel Fernández <daniel.fernandez@11thlabs.org>
2015-05-30 15:46:21 +0200

    Made room for the new org.thymeleaf.engine package in 3.0

---
+++ src/test/java/org/thymeleaf/templateengine/stsm/STSMTest.java
@@ -0,0 +1,73 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2014, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.templateengine.stsm;
+
+import java.util.Arrays;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.dialect.IDialect;
+import org.thymeleaf.templateengine.stsm.context.STSMWebProcessingContextBuilder;
+import org.thymeleaf.testing.templateengine.context.web.SpringWebProcessingContextBuilder;
+import org.thymeleaf.testing.templateengine.engine.TestExecutor;
+import org.thymeleaf.tests.util.SpringSpecificVersionUtils;
+
+
+public class STSMTest {
+    
+    
+    public STSMTest() {
+        super();
+    }
+    
+    
+    
+    
+    @Test
+    public void testSTSMWithoutIntegratedConversion() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.setProcessingContextBuilder(new STSMWebProcessingContextBuilder());
+        executor.setDialects(Arrays.asList(new IDialect[] { SpringSpecificVersionUtils.createSpringStandardDialectInstance()}));
+        executor.execute("classpath:templateengine/stsm");
+        
+        Assert.assertTrue(executor.isAllOK());
+        
+        
+    }
+
+    @Test
+    public void testSTSMWithIntegratedConversion() throws Exception {
+
+        final SpringWebProcessingContextBuilder contextBuilder = new SpringWebProcessingContextBuilder();
+        contextBuilder.setApplicationContextConfigLocation("classpath:templateengine/stsm/applicationContext.xml");
+
+        final TestExecutor executor = new TestExecutor();
+        executor.setProcessingContextBuilder(contextBuilder);
+        executor.setDialects(Arrays.asList(new IDialect[] { SpringSpecificVersionUtils.createSpringStandardDialectInstance()}));
+        executor.execute("classpath:templateengine/stsm");
+
+        Assert.assertTrue(executor.isAllOK());
+
+
+    }
+
+
+}
