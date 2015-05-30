c62ada9a5a26bebd267f350cfe8ed35ebc923fe2
Daniel Fernández <daniel.fernandez@11thlabs.org>
2015-05-30 13:38:17 +0200

    Merged some 3.0 tests, made room for the new org.thymeleaf.engine package in 3.0

---
+++ src/test/java/org/thymeleaf/templateengine/dataprefix/attrprocessors/DataPrefixAttrProcessorsTest.java
@@ -0,0 +1,205 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2014, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.templateengine.dataprefix.attrprocessors;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.testing.templateengine.engine.TestExecutor;
+
+
+public class DataPrefixAttrProcessorsTest {
+
+
+    public DataPrefixAttrProcessorsTest() {
+        super();
+    }
+
+
+
+
+
+
+    @Test
+    public void testRemove() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/remove");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testIf() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/if");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testUnless() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/unless");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testInline() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/inline");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testInclude() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/include");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testSubstituteby() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/substituteby");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testReplace() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/replace");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testEach() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/each");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testObject() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/object");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testAttr() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/attr");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    @Test
+    public void testSimpleValue() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/simplevalue");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testDoubleValue() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/doublevalue");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testAppendPrepend() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/appendprepend");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testFixedValue() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/fixedvalue");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testSwitch() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/switch");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testWith() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/dataprefix/attrprocessors/with");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+}
---
+++ src/test/java/org/thymeleaf/templateengine/features/FeaturesTest.java
@@ -0,0 +1,143 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2014, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.templateengine.features;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.testing.templateengine.engine.TestExecutor;
+
+
+
+
+
+public class FeaturesTest {
+    
+    
+    public FeaturesTest() {
+        super();
+    }
+    
+    
+    
+    
+    @Test
+    public void testText() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/text");
+        
+        Assert.assertTrue(executor.isAllOK());
+        
+    }
+    
+    
+    
+    @Test
+    public void testLink() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/link");
+        
+        Assert.assertTrue(executor.isAllOK());
+        
+    }
+
+
+    @Test
+    public void testUtil() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/util");
+        
+        Assert.assertTrue(executor.isAllOK());
+        
+    }
+
+
+    @Test
+    public void testExpression() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/expression");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+    
+    @Test
+    public void testMessages() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/messages");
+        
+        Assert.assertTrue(executor.isAllOK());
+        
+    }
+
+
+    @Test
+    public void testRequest() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/request");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testNormalization() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/normalization");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testExecInfo() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/execinfo");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+    @Test
+    public void testAccessRestrictions() throws Exception {
+
+        final TestExecutor executor = new TestExecutor();
+        executor.execute("classpath:templateengine/features/accessrestrictions");
+
+        Assert.assertTrue(executor.isAllOK());
+
+    }
+
+
+
+
+
+}
