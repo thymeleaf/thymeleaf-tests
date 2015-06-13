d641baa5952373c6ef1173742cfce24eb116abdd
Daniel Fernández <daniel.fernandez@11thlabs.org>
2015-06-13 20:12:03 +0200

    Moved HTML and XML parsers to a "markup" subpackage in order to make room for the "text" parsing mechanism

--- src/test/java/org/thymeleaf/engine/ElementProcessorIteratorTest.java
+++ src/test/java/org/thymeleaf/engine/ElementProcessorIteratorTest.java
@@ -31,8 +31,8 @@ import org.thymeleaf.dialect.IProcessorDialect;
 import org.thymeleaf.model.IOpenElementTag;
 import org.thymeleaf.resource.StringResource;
 import org.thymeleaf.templatemode.TemplateMode;
-import org.thymeleaf.templateparser.HTMLTemplateParser;
-import org.thymeleaf.templateparser.XMLTemplateParser;
+import org.thymeleaf.templateparser.markup.HTMLTemplateParser;
+import org.thymeleaf.templateparser.markup.XMLTemplateParser;
 
 
 public final class ElementProcessorIteratorTest {
