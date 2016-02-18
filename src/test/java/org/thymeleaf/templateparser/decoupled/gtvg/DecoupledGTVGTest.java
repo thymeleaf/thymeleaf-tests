95c42f6b3a873e8c1a941c359161069012a5f05d
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-02-18 22:20:28 +0100

    Added decoupled-templates version of GTVG home page as a test for the decoupled templates mechanism

---
+++ src/test/java/org/thymeleaf/templateparser/decoupled/gtvg/DecoupledGTVGTest.java
@@ -0,0 +1,95 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2014, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.templateparser.decoupled.gtvg;
+
+import java.util.Calendar;
+import java.util.Map;
+
+import org.junit.Assert;
+import org.junit.Test;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.context.IExpressionContext;
+import org.thymeleaf.linkbuilder.StandardLinkBuilder;
+import org.thymeleaf.templateresolver.ClassLoaderTemplateResolver;
+import org.thymeleaf.testing.templateengine.util.ResourceUtils;
+import org.thymeleaf.util.ClassLoaderUtils;
+
+
+public class DecoupledGTVGTest {
+
+
+
+    public DecoupledGTVGTest() {
+        super();
+    }
+    
+
+
+    
+    @Test
+    public void testDecoupledGTVG() throws Exception {
+
+
+        final ClassLoaderTemplateResolver templateResolver = new ClassLoaderTemplateResolver();
+
+        templateResolver.setTemplateMode("HTML");
+        templateResolver.setPrefix("templateparser/decoupled/gtvg/templates/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setUseDecoupledLogic(true);
+
+
+        final TemplateEngine templateEngine = new TemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver);
+        templateEngine.setLinkBuilder(new StandardLinkBuilder() {
+            @Override
+            protected String computeContextPath(final IExpressionContext context, final String base, final Map<String, Object> parameters) {
+                return "";
+            }
+        });
+
+
+
+        final Context ctx = new Context();
+        ctx.setVariable("user", new User("John", "Apricot", "Antarctica", null));
+        ctx.setVariable("today", Calendar.getInstance());
+
+
+        final String result = templateEngine.process("home", ctx);
+
+
+        final String expected =
+                ResourceUtils.read(
+                        ClassLoaderUtils.getClassLoader(DecoupledGTVGTest.class).getResourceAsStream("templateparser/decoupled/gtvg/result/home.html"),
+                        "ISO-8859-1",
+                        true);
+
+        Assert.assertEquals(ResourceUtils.normalize(expected), ResourceUtils.normalize(result));
+
+
+    }
+
+
+
+
+
+
+
+}
