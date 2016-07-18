454922f7247e50486b8dbd56ed949929c778f672
Daniel Fernández <daniel.fernandez@11thlabs.org>
2016-07-19 00:37:17 +0200

    Improved tests for thymeleaf/thymeleaf#518 - two no-op processors, changes on local variables

--- src/test/java/org/thymeleaf/templateengine/processors/dialects/noop/NoOp2ModelProcessor.java
+++ src/test/java/org/thymeleaf/templateengine/processors/dialects/noop/NoOp2ModelProcessor.java
@@ -20,25 +20,28 @@
 package org.thymeleaf.templateengine.processors.dialects.noop;
 
 import org.thymeleaf.context.ITemplateContext;
-import org.thymeleaf.engine.AttributeName;
-import org.thymeleaf.model.IProcessableElementTag;
-import org.thymeleaf.processor.element.AbstractAttributeTagProcessor;
-import org.thymeleaf.processor.element.IElementTagStructureHandler;
+import org.thymeleaf.model.IModel;
+import org.thymeleaf.processor.element.AbstractElementModelProcessor;
+import org.thymeleaf.processor.element.IElementModelStructureHandler;
 import org.thymeleaf.templatemode.TemplateMode;
 
-public class NoOpAttributeTagProcessor extends AbstractAttributeTagProcessor {
+public class NoOp2ModelProcessor extends AbstractElementModelProcessor {
 
-    private static final int PRECEDENCE = 1000;
+    private static final int PRECEDENCE = 1100;
 
 
-    public NoOpAttributeTagProcessor(final String dialectPrefix) {
-        super(TemplateMode.HTML, dialectPrefix, null, false, "noop", true, PRECEDENCE, false);
+    public NoOp2ModelProcessor(final String dialectPrefix) {
+        super(TemplateMode.HTML, dialectPrefix, "noop", true, null, false, PRECEDENCE);
     }
 
+
     @Override
-    protected void doProcess(final ITemplateContext context, final IProcessableElementTag tag, final AttributeName attributeName, final String attributeValue, final IElementTagStructureHandler structureHandler) {
+    protected void doProcess(final ITemplateContext context, final IModel model, final IElementModelStructureHandler structureHandler) {
         // Nothing to do, that's the idea. Neither to do anything, nor to change the tag in any way
-        structureHandler.setLocalVariable("noop-tag", Boolean.TRUE);
+        final Boolean var = (Boolean) context.getVariable("noop-model");
+        if (var == null || !var.booleanValue()) {
+            throw new RuntimeException("Local variable has not reached from one no-op model operator to the next one");
+        }
     }
 
 }
