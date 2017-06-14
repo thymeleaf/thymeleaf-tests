4734f403102b981920fb2ad1a5ac42a91ec62ad1
Daniel Fernández <daniel.fernandez@11thlabs.org>
2017-06-14 23:32:56 +0200

    Added specific implementation of a test reporter (minimizing output so that TravisCI does not complain so much)

---
+++ src/main/java/org/thymeleaf/tests/util/TestExecutorFactory.java
@@ -0,0 +1,45 @@
+/*
+ * =============================================================================
+ * 
+ *   Copyright (c) 2011-2017, The THYMELEAF team (http://www.thymeleaf.org)
+ * 
+ *   Licensed under the Apache License, Version 2.0 (the "License");
+ *   you may not use this file except in compliance with the License.
+ *   You may obtain a copy of the License at
+ * 
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ *   Unless required by applicable law or agreed to in writing, software
+ *   distributed under the License is distributed on an "AS IS" BASIS,
+ *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *   See the License for the specific language governing permissions and
+ *   limitations under the License.
+ * 
+ * =============================================================================
+ */
+package org.thymeleaf.tests.util;
+
+
+import org.thymeleaf.testing.templateengine.engine.TestExecutor;
+import org.thymeleaf.testing.templateengine.report.ITestReporter;
+import org.thymeleaf.tests.testreporter.MinimalConsoleTestReporter;
+
+public final class TestExecutorFactory {
+
+    private static final ITestReporter MINIMAL_TEST_REPORTER = new MinimalConsoleTestReporter();
+
+
+    public static TestExecutor createTestExecutor() {
+        final TestExecutor testExecutor = new TestExecutor();
+        testExecutor.setReporter(MINIMAL_TEST_REPORTER);
+        return testExecutor;
+    }
+
+
+
+    private TestExecutorFactory() {
+        super();
+    }
+
+
+}
